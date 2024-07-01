29924e9341f87a76e04e100c8bba647e1fa79f01
binushasherin <binushasherini@gmail.com>
2024-07-01 22:39:25 +0530

    first commit

---
+++ 02_JS_variables/Ch2_pgm_02.js
@@ -0,0 +1,15 @@
+var score
+
+score = 100;
+
+console.log(score);
+var score=100;
+
+console.log(score);
+var score=200;
+
+console.log(score);
+var score=300;
+
+console.log(score);
+var score=400;
